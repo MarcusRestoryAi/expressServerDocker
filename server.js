15563038f92e4005510a1ed76e18a88193bc31d6
Marcus <marcus.henriksson@restory.ai>
2025-09-17 10:00:06 +0200

    init comit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+// Simple GET endpoint
+app.get('/', (req, res) => {
+  res.send('Hello from Express server on port 8080!');
+});
+
+// Listen method for startup
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
