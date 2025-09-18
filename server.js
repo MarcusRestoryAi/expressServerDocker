bed8f02403d5d79e74d7470af4c9e6d8213c32bc
Marcus <marcus.henriksson@restory.ai>
2025-09-18 10:45:49 +0200

    Added Azure message

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const PORT = 8080;
 
 // Simple GET endpoint
 app.get('/', (req, res) => {
-  res.send('Hello from Express server on port 8080!');
+  res.send('Hello from Express server on port 8080, Azure Edition!');
 });
 
 // Listen method for startup
